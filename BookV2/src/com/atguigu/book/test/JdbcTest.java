230643b89ea40b7ff6cea5986512c1ff9ff60f35
SuperCoder1999 <331150871@qq.com>
2021-11-19 22:18:31 +0800

    1

---
+++ BookV2/src/com/atguigu/book/test/JdbcTest.java
@@ -0,0 +1,5 @@
+package com.atguigu.book.test;
+
+public class JdbcTest {
+    
+}
---
+++ BookV2/src/com/atguigu/book/utils/JdbcUtils.java
@@ -0,0 +1,55 @@
+package com.atguigu.book.utils;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JdbcUtils {
+    private static DruidDataSource dataSource;//这里 韩顺平老师 的是DataSource类
+
+    static {
+        Properties properties = null;
+        //获取 JdbcUtils 属性配置文件 jdbc.properties
+        InputStream resourceAsStream = JdbcUtils.class.getClassLoader().getResourceAsStream("jdbc.properties");
+        try {
+            properties.load(resourceAsStream);
+            dataSource = (DruidDataSource)DruidDataSourceFactory.createDataSource(properties);
+            //需要向下转型
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取数据库连接池中的连接
+     * @return 如果返回null,说明获取连接失败<br/>有值就是获取连接成功
+     */
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+
+    /**
+     * 关闭连接,将连接放回数据库连接池中
+     * @param connection
+     */
+    public void close(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+}
