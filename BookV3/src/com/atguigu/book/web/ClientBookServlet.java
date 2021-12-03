7dedf5b089c4375f83a63e4cbefa3f5f5c8e52d2
SuperCoder1999 <331150871@qq.com>
2021-12-03 23:15:25 +0800

    图书管理系统第五阶段完成

--- BookV3/src/com/atguigu/book/web/ClientBookServlet.java
+++ BookV3/src/com/atguigu/book/web/ClientBookServlet.java
@@ -50,7 +50,7 @@ public class ClientBookServlet extends BaseServlet {
 //        page.setUrl(url.toString());
         //我觉得,可以直接 将 page()删除了,直接 page.setUrl("client/bookServlet?action=pageByPrice&min="+min+"&max="+max);
         //因为page显示的页面就是, min如果为空max为空,即查询[0,0xffffffff]的范围.
-        //而 如果min或max单方面为空,也不会报错,和不传入min或max参数是一样的.
+        //而 如果min或max单方面为空,也不会报错,和不传入min或max参数是一样的
         page.setUrl("client/bookServlet?action=pageByPrice&min="+min+"&max="+max);
 
         //4.请求转发到 /pages/manager/book_manager.jsp 让jsp利用page中的信息显示页面
